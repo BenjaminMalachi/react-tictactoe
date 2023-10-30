c44f2afe17f8bc6e11f91c7bd65078321533f6a6
alexpanmk <alexpanmk@gmail.com>
2023-10-30 16:31:06 +0800

    style: modify app css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { Flex, Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Flex
+      width="100vw"
+      height="100vh"
+      align="center" // for vertical alignment
+      justify="center" // fo
+    >
+      <Grid
+        p={10}
+        w={"680px"}
+        h={"680px"}
+        templateColumns="repeat(3, 1fr)"
+        templateRows="repeat(3, 1fr)"
+        gap={20}
+      >
+        <GridItem borderRadius="20px" w="100%" h="100%" bg="white" />
+        <GridItem borderRadius="20px" w="100%" h="100%" bg="white" />
+        <GridItem borderRadius="20px" w="100%" h="100%" bg="white" />
+        <GridItem borderRadius="20px" w="100%" h="100%" bg="white" />
+        <GridItem borderRadius="20px" w="100%" h="100%" bg="white" />
+        <GridItem borderRadius="20px" w="100%" h="100%" bg="white" />
+        <GridItem borderRadius="20px" w="100%" h="100%" bg="white" />
+        <GridItem borderRadius="20px" w="100%" h="100%" bg="white" />
+        <GridItem borderRadius="20px" w="100%" h="100%" bg="white" />
+      </Grid>
+    </Flex>
+  );
 }
 
-export default App
+export default App;
