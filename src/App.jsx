1f0d3fa330108b2bcd4106ec493b9be6b2503f9d
alexpanmk <alexpanmk@gmail.com>
2023-11-02 00:40:24 +0800

    refactor: add .jsx to import path

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import {
   togglePlayer,
   checkWinner,
   resetGame,
-} from "./GameLogic";
+} from "./GameLogic.js";
 
 import { Box, Button, Flex, Grid, GridItem } from "@chakra-ui/react";
-import GameBoard from "./components/GameBoard";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
   //State
-  const [boardState, setBoardState] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [boardState, setBoardState] = useState([0, 1, 0, 0, 0, 0, 0, 0, 0]);
   const [message, setMessage] = useState("TIC TAC TOE");
   const [turnCount, setTurnCount] = useState(0);
   const [currentPlayer, setCurrentPlayer] = useState(1);
@@ -22,33 +22,31 @@ function App() {
 
   //Event Handlers
 
-  // const handleClick = (index, value) => {
-  //   console.log(index, value);
-
-  //   //Check if square is empty
-  // };
+  const handleClick = (index, value) => {
+    console.log(index, value);
+  };
 
   const resetGameHandler = () => {
     console.log("Reset");
   };
 
   //When Player Clicks a grid
-  const handleClick = (index, value) => {
-    if (!boardState[index] && !winner) {
-      const updatedBoard = handleCellClick(
-        [...boardState],
-        index,
-        currentPlayer
-      );
-      setBoardState(updatedBoard);
-      const newWinner = checkWinner(updatedBoard, currentPlayer);
-      if (newWinner) {
-        setWinner(newWinner);
-      } else {
-        togglePlayer(currentPlayer, setCurrentPlayer);
-      }
-    }
-  };
+  // const handleClick = (index, value) => {
+  //   if (!boardState[index] && !winner) {
+  //     const updatedBoard = handleCellClick(
+  //       [...boardState],
+  //       index,
+  //       currentPlayer
+  //     );
+  //     setBoardState(updatedBoard);
+  //     const newWinner = checkWinner(updatedBoard, currentPlayer);
+  //     if (newWinner) {
+  //       setWinner(newWinner);
+  //     } else {
+  //       togglePlayer(currentPlayer, setCurrentPlayer);
+  //     }
+  //   }
+  // };
 
   // const resetGameHandler = () => {
   //   resetGame(setCurrentPlayer, setBoardState, setWinner, setMessage);
